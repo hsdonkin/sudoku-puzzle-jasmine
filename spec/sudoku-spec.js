70e024f01f0124b4a78879c2bd0dd1b3947ca26b
Student <example@example.com>
2019-06-25 14:22:38 -0700

    finally figured out if kitty was a number or not

--- spec/sudoku-spec.js
+++ spec/sudoku-spec.js
@@ -1,25 +1,28 @@
-import { myPuzzle } from './../src/sudoku.js'
+import { myPuzzle, SudokuPuzzle } from './../src/sudoku.js'
 
 
 describe ('sudoku', function(){
   it ('should test whether something is or is not a number', function(){
-    /* eslint-disable */
-    expect(myPuzzle.grid[0][0]).toEqual(jasmine.any(Number));
-    /* eslint-enable */
+    myPuzzle.numberCheck();
+    expect(myPuzzle.valid).toEqual(true);
   });
-
 });
 
-describe ('sudoku', function(){
-  it ('test whether all entries in an array are numbers', function(){
-    /* eslint-disable */
-    console.log(myPuzzle.grid[0]);
-    myPuzzle.grid[0].forEach(function(arrayEntry){
-      console.log(arrayEntry);
-      expect(arrayEntry).toEqual(jasmine.any(Number));
 
-    });
-    /* eslint-enable */ 
-  });
 
-});
+// describe ('sudoku', function(){
+//   it ('test whether all entries in an array are numbers', function(){
+//     /* eslint-disable */
+//     console.log(myPuzzle.grid[0]);
+//     myPuzzle.grid.forEach(function(sudokuRow) {
+//       sudokuRow.forEach(function(arrayEntry){
+//         console.log(arrayEntry);
+//         expect(arrayEntry).toEqual(jasmine.any(Number));
+//
+//     });
+//
+//     });
+//     /* eslint-enable */
+//   });
+//
+// });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,6 +1,35 @@
-function SudokuPuzzle(inputGrid) {
+export function SudokuPuzzle(inputGrid) {
   this.grid = inputGrid;
+  this.valid = true;
 }
+
+SudokuPuzzle.prototype.numberCheck = function(){
+  for (var row = 0; row < this.grid.length; row++) {
+    for (var col = 0; col < this.grid[row].length; col++) {
+      console.log(this.grid[row][col]);
+      console.log(this.valid);
+      if (isNaN(this.grid[row][col]) === true) {
+        return this.valid = false;
+      } else {
+        this.valid = true;
+      }
+    }
+  }
+}
+
+
+/* eslint-disable */
+// SudokuPuzzle.prototype.numberCheck = function(){
+//   this.grid.forEach(function(array){
+//     array.forEach(function(entry){
+//
+//     console.log(entry);
+//
+//     });
+//   })
+// }
+/* eslint-enable */
+
 
 
 
@@ -17,4 +46,7 @@ var sampleRow9 = [1,2,3,4,5,6,7,8,9];
 var sampleGrid = [sampleRow1, sampleRow2, sampleRow3, sampleRow4, sampleRow5, sampleRow6, sampleRow7, sampleRow8, sampleRow9];
 
 export var myPuzzle = new SudokuPuzzle(sampleGrid);
-console.log("Test ");
+
+
+
+// export var puzzleTest = myPuzzle.numberCheck();
